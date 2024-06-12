c0e1855bfdf927f46c1a5c7378dedd013e783a47
Antonino <drakon9927@libero.it>
2024-06-12 15:36:00 +0200

    m2

--- generate_password.php
+++ generate_password.php
@@ -1,15 +1,22 @@
 <?php
 // Se la lunghezza è stata inviata tramite GET
 if (isset($_GET['length'])) {
-    $length = intval($_GET['length']);  
-    $password = "";  
+    $length = intval($_GET['length']);  // Assicura che la lunghezza sia un intero
 
-    // Aggiunge un 'X' alla stringa per ogni iterazione fino a raggiungere la lunghezza specificata
+    // Definizione dei caratteri consentiti
+    $allowedChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=[]{}|;:,.<>?';
+    $password = "";  // Inizia con una stringa vuota
+
+    // Ottiene il numero di caratteri nella stringa dei caratteri consentiti
+    $numAllowedChars = strlen($allowedChars);
+
+    // Aggiunge un carattere casuale alla stringa per ogni iterazione
     for ($i = 0; $i < $length; $i++) {
-        $password .= 'X';
+        $randomIndex = rand(0, $numAllowedChars - 1);  
+        $password .= $allowedChars[$randomIndex];  
     }
 
-    // Stampa la password generata
+   
     echo "Password generata: " . htmlspecialchars($password);
 } else {
     // Se non è stato inviato nessun dato, reindirizza l'utente al form
